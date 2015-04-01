1adac9fd5abb9b5ed10c6fd572e3b89721a632cc
Tim Cooper <tjkoopa@gmail.com>
2015-04-02 00:56:39 +0100

    Register field type

--- Extension.php
+++ Extension.php
@@ -8,6 +8,15 @@ use Bolt\BaseExtension;
 class Extension extends BaseExtension
 {
 
+    public function __construct(Application $app)
+    {
+        parent::__construct($app);
+        $this->app['config']->fields->addField(new ColourSpectrumField());
+        if ($this->app['config']->getWhichEnd()=='backend') {
+            $this->app['htmlsnippets'] = true;
+            $this->app['twig.loader.filesystem']->prependPath(__DIR__."/twig");
+        }
+    }
 
     public function initialize() {
         $this->addCss('assets/spectrum/spectrum.css');
